3621695046321c8122ff947d7cd182376bd97107
DenVoronin <92761907+DenVoronin@users.noreply.github.com>
2021-11-14 12:29:10 +0200

    Delete ibs-trainee-spring-security-jwt-master directory

--- ibs-trainee-spring-security-jwt-master/src/main/java/ru/ibs/trainee/spring/securityjwt/auth/ApplicationUserDao.java
+++
@@ -1,8 +0,0 @@
-package ru.ibs.trainee.spring.securityjwt.auth;
-
-import java.util.Optional;
-
-public interface ApplicationUserDao {
-
-    Optional<ApplicationUser> selectUserFromDbByUserName(String username);
-}
